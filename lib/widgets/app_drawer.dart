3951c9e4bc4768dd59c145afa85a5620204e0571
Hudhayfa <chiefhudhayfa@gmail.com>
2021-05-31 06:59:12 +0530

    added animations

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/helpers/custom_route.dart';
 
+import '../helpers/custom_route.dart';
 import '../screens/orders_screen.dart';
 import '../screens/user_products_screen.dart';
 import '../providers/auth.dart';
@@ -29,8 +31,13 @@ class AppDrawer extends StatelessWidget {
             leading: Icon(Icons.credit_card),
             title: Text("Orders"),
             onTap: () {
-              Navigator.of(context)
-                  .pushReplacementNamed(OrdersScreen.routeName);
+              // Navigator.of(context)
+              // .pushReplacementNamed(OrdersScreen.routeName);
+              Navigator.of(context).pushReplacement(
+                CustomRoute(
+                  builder: (ctx) => OrdersScreen(),
+                ),
+              );
             },
           ),
           Divider(),
--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -20,30 +20,38 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text("\$${widget.order.amount}"),
-            subtitle: Text(
-              DateFormat.MMMEd().format(widget.order.dateTime),
-            ),
-            trailing: IconButton(
-                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-                onPressed: () {
-                  setState(() {
-                    _expanded = !_expanded;
-                  });
-                }),
-          ),
-          if (_expanded)
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(
-                widget.order.products.length * 20.0 + 50,
-                180,
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeIn,
+      height: _expanded
+          ? min(
+              widget.order.products.length * 20.0 + 120,
+              200,
+            )
+          : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text("\$${widget.order.amount}"),
+              subtitle: Text(
+                DateFormat.MMMEd().format(widget.order.dateTime),
               ),
+              trailing: IconButton(
+                  icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                  onPressed: () {
+                    setState(() {
+                      _expanded = !_expanded;
+                    });
+                  }),
+            ),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: _expanded
+                  ? min(widget.order.products.length * 20.0 + 50, 100)
+                  : 0,
               child: ListView(
                 children: widget.order.products
                     .map((prod) => Container(
@@ -69,7 +77,8 @@ class _OrderItemState extends State<OrderItem> {
                     .toList(),
               ),
             )
-        ],
+          ],
+        ),
       ),
     );
   }
